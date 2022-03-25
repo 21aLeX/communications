d329fabaeb7942cf9444fc1d8eb2ae2597dfac2e
alex <aleksandra12mail@mail.ru>
2022-03-25 16:28:23 +0800

    update

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import s from './Style/Login.module.css'
@@ -7,50 +7,69 @@ import s from './Style/Login.module.css'
 const Login = (props) => {
     const [login, setLogin] = useState('')
     const [password, setPassword] = useState('')
+    const [btn, setBtn] = useState(true)
 
-    const handleLogin =(e)=>{
-        console.log('input>',e.target.value)
-        setLogin(e.target.value)
+    const handleState = (e) => { 
+            checkBtn()     
+        if (e.target.id == 'mui-1')
+            setLogin(e.target.value)
+        if (e.target.id == 'mui-2')
+            setPassword(e.target.value)      
+    }
+    const checkBtn = (e)=>{
+        
+        if(login && password)
+        setBtn(false)
+        if(!login || !password)
+        setBtn(true)
+    }
+    const checkState = (e) => {
+        if (!login || !password)
+            e.target.classList.add(s.color)
+        if (login || password)
+            e.target.classList.remove(s.color)
 
     }
-    const handleSubmit=(e)=>{
-e.preventDefault();
-console.log('submit>', login)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('submit>', login)
     }
     return (
         <div className={s.container} >
-        <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <TextField
-                    value={login}
-                    onChange={handleLogin}
+                        value={login}
+                        onBlur={checkState}
+                        onChange={handleState}
                         color="secondary"
                         label="Login"
                         variant="standard"
                     /></div>
                 <div> <TextField
                     value={password}
-                    onChange={e=>setPassword(e.target.value)}
+                    onBlur={checkState}
+                    onChange={handleState}
                     color="secondary"
                     label="Password"
                     type="password"
                     autoComplete="current-password"
                     variant="standard"
                 /></div>
-                    <Button type='submit' variant="outlined" color="secondary"
-                        style={{
-                            top: 5
-                        }}
-                    >Sign in</Button>
-    </form>
+                <Button disabled={btn} type='submit' variant="outlined" color="secondary"
+                    style={{
+                        top: 5
+                    }}
+                >Sign in</Button>
+            </form>
         </div>);
 
-    
+
 };
-function mapStateToProps(state){
-        console.log('mapStateToProps>',state)
-        return{
-            login:state.login
-        }
+function mapStateToProps(state) {
+    console.log('mapStateToProps>', state)
+    return {
+        login: state.login
     }
+}
 export default connect(mapStateToProps)(Login)
